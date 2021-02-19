79fa5a9b99d52ec816eaa044b1ad6da4cca152ae
hassantvs24 <hassantvs24@gmail.com>
2021-02-19 21:48:00 +0600

    Model Add

---
+++ app/Models/Package.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property string $name
+ * @property float $hours
+ * @property float $price
+ * @property boolean $isCoupon
+ * @property string $description
+ * @property string $expire
+ * @property string $deleted_at
+ * @property string $created_at
+ * @property string $updated_at
+ * @property Purchase[] $purchases
+ */
+class Package extends Model
+{
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'hours', 'price', 'isCoupon', 'description', 'expire', 'deleted_at', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function purchases()
+    {
+        return $this->hasMany('App\Models\Purchase');
+    }
+}
