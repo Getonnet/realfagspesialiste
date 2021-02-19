488cd17c382fb7db549dfef96893ce6849b56d38
hassantvs24 <hassantvs24@gmail.com>
2021-02-19 21:38:21 +0600

    Update Auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,12 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
     ]);
 mix.browserSync('http://127.0.0.1:8000');
+
+if (mix.inProduction()) {
+    mix.version();
+}
