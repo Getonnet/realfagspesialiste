b66f37fab0fcbbae22a2f4a033ea38b8eeb03f08
hassantvs24 <hassantvs24@gmail.com>
2021-02-19 22:00:40 +0600

    Update user Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'user_type',
+        'register_ref',
+        'profile_photo_path'
     ];
 
     /**
