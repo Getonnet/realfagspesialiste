2e4dd92ebad73d39c44a3f765c26669d3e3d7c24
hassantvs24 <hassantvs24@gmail.com>
2021-03-02 13:16:32 +0600

    Update Crose

--- config/naz.php
+++ config/naz.php
@@ -3,7 +3,7 @@ return [
     'save' => ['message' => 'It Works Well! Save Data Successfully',  'type' => 'success'],
     'edit' => ['message' => 'It Works Well! Edit Data Successfully',  'type' => 'success'],
     'del' => ['message' => 'It Works Well! Delete Data Successfully',  'type' => 'success'],
-    'error' => ['message' => 'Oh! Something error here.',  'alert' => 'type'],
+    'error' => ['message' => 'Oh! Something error here.',  'type' => 'error'],
     'db_error' => ['message' => 'Oh! Database error. Please submit proper data.',  'type' => 'error'],
 
 ];
