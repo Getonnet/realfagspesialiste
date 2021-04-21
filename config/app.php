eb21c923896ddd5811caf81dc1b9374672c6b7d3
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-04-21 17:21:12 +0600

    Update app.php

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Dhaka',//'Europe/Oslo',
+    'timezone' => 'Europe/Oslo',//'Asia/Dhaka',
 
     /*
     |--------------------------------------------------------------------------
