36c09f02cbed4a61b04c0bb6b2e0a00baac1f3ad
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-07-07 17:15:26 +0600

    update teacher

--- resources/views/frontend/teacher/overview_mini.blade.php
+++ resources/views/frontend/teacher/overview_mini.blade.php
@@ -1,12 +1,14 @@
 @php
     $hour = $table->student->purchase()->where('status', 'Active')->sum('hours');
     $spends = $table->student->time_log()->where('status', 'End')->get();
+    $travel_times = 0;
     $spend_times = 0;
     foreach ($spends as $spend){
         $spend_times += $spend->spend_time();
+        $travel_times += $spend->hour_spend;
     }
     $hour_to_min = $hour * 60;
-    $remain_min = $hour_to_min - $spend_times;
+    $remain_min = $hour_to_min - ($spend_times + $travel_times);
     $remaining = number_format(($remain_min / 60), 2, '.', ' ');
 
 //dd($remaining);
