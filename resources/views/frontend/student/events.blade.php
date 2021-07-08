a1dc84bd42b989c123c3729dccfff9405f848e8e
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-07-08 12:17:51 +0600

    update tab

--- resources/views/frontend/student/events.blade.php
+++ resources/views/frontend/student/events.blade.php
@@ -12,42 +12,42 @@
             <x-card title="{{__('Events List')}}">
                 <table class="table table-separate table-head-custom table-sm table-striped" id="kt_datatable">
                     <thead>
-                    <tr>
-                        <th>{{__('Event Date')}}</th>
-                        <th>{{__('Title')}}</th>
-                        <th>{{__('Subject')}}</th>
-                        <th>{{__('Teacher')}}</th>
-                        <th>{{__('Start')}}</th>
-                        <th>{{__('End')}}</th>
-                        <th>{{__('Hour')}}</th>
-                        <th>{{__('Travel')}}</th>
-                        <th class="text-right">{{__('Action')}}</th>
-                    </tr>
+                        <tr>
+                            <th>{{__('Event Date')}}</th>
+                            <th>{{__('Title')}}</th>
+                            <th>{{__('Subject')}}</th>
+                            <th>{{__('Teacher')}}</th>
+                            <th>{{__('Start')}}</th>
+                            <th>{{__('End')}}</th>
+                            <th>{{__('Hour')}}</th>
+                            <th>{{__('Travel')}}</th>
+                            <th class="text-right">{{__('Action')}}</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    @foreach($table as $row)
-                        <tr>
-                            <td data-sort="{{strtotime($row->event_start)}}">{{date('d.M.Y', strtotime($row->event_start))}}</td>
-                            <td class="{{$row->status != 'End' ? 'text-primary': ''}}">{{$row->name ?? __('No Title')}}</td>
-                            <td>{{$row->subject_name}}</td>
-                            <td>{{$row->teacher_name}}</td>
-                            <td>{{isset($row->start_time) ? date('d, M H:i', strtotime($row->start_time)) : ''}}</td>
-                            <td>{{isset($row->end_time) ? date('d, M H:i', strtotime($row->end_time)) : ''}}</td>
-                            <td>{{$row->spend_time('H')}}</td>
-                            <td>{{$row->hour_spend}} Min</td>
-                            <td class="text-right">
-                                <x-actions>
-                                    <li class="navi-item">
-                                        <a href="javascript:;" class="navi-link" data-toggle="modal" data-target="#viewModal" onclick="viewFn(this)"
-                                           data-href="{{route('student.events-overview', ['id' => $row->id])}}">
-                                            <span class="navi-icon"><i class="fab la-phabricator text-info"></i></span>
-                                            <span class="navi-text">{{__('View')}}</span>
-                                        </a>
-                                    </li>
-                                </x-actions>
-                            </td>
-                        </tr>
-                    @endforeach
+                        @foreach($table as $row)
+                            <tr>
+                                <td data-sort="{{strtotime($row->event_start)}}">{{date('d.M.Y', strtotime($row->event_start))}}</td>
+                                <td class="{{$row->status != 'End' ? 'text-primary': ''}}">{{$row->name ?? __('No Title')}}</td>
+                                <td>{{$row->subject_name}}</td>
+                                <td>{{$row->teacher_name}}</td>
+                                <td>{{isset($row->start_time) ? date('d, M H:i', strtotime($row->start_time)) : ''}}</td>
+                                <td>{{isset($row->end_time) ? date('d, M H:i', strtotime($row->end_time)) : ''}}</td>
+                                <td>{{$row->spend_time('H')}}</td>
+                                <td>{{$row->hour_spend}} Min</td>
+                                <td class="text-right">
+                                    <x-actions>
+                                        <li class="navi-item">
+                                            <a href="javascript:;" class="navi-link" data-toggle="modal" data-target="#viewModal" onclick="viewFn(this)"
+                                               data-href="{{route('student.events-overview', ['id' => $row->id])}}">
+                                                <span class="navi-icon"><i class="fab la-phabricator text-info"></i></span>
+                                                <span class="navi-text">{{__('View')}}</span>
+                                            </a>
+                                        </li>
+                                    </x-actions>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </x-card>
